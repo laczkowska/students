bcc8586b1795b5378372a0bcfba25db91b2d30fa
s24545 <s24545@pjwstk.edu.pl>
2023-10-12 15:58:07 +0200

    project initialization

---
+++ src/main/java/com/example/students121023/Students121023Application.java
@@ -0,0 +1,13 @@
+package com.example.students121023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Students121023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Students121023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/students121023/Students121023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.students121023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Students121023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
